ef02a93ed491d9611e93222c01640c97431420d3
AlarKING5126 <santiago5126alarcon@gmail.com>
2025-09-01 10:40:36 -0500

    Archivo repositorio creado

--- src/main/java/com/biblioteca/bibliotecacrud/repository/LibroRepository.java
+++ src/main/java/com/biblioteca/bibliotecacrud/repository/LibroRepository.java
@@ -1,4 +1,8 @@
 package com.biblioteca.bibliotecacrud.repository;
 
-public interface LibroRepository {
+import com.biblioteca.bibliotecacrud.entity.Libro;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LibroRepository extends JpaRepository<Libro,Integer> {
+    boolean existsByLibro(String Libro);
 }
