121957e17a565e62421e19dc1cd83d3351a42418
Ninjaky22 <juan_restrepope@fet.edu.co>
2025-09-01 17:45:08 -0500

    Creacion y validacion LibroService

--- src/main/java/com/biblioteca/bibliotecacrud/repository/ILibroRepo.java
+++ src/main/java/com/biblioteca/bibliotecacrud/repository/ILibroRepo.java
@@ -3,7 +3,7 @@ package com.biblioteca.bibliotecacrud.repository;
 import com.biblioteca.bibliotecacrud.entity.Libro;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ILibroRepo extends JpaRepository<Libro, Integer> {
+public interface ILibroRepo extends JpaRepository<Libro, Long> {
     //Verifica si hay un libro con el mismo titulo
     boolean existsByLibro(String Libro);
 }
--- src/main/java/com/biblioteca/bibliotecacrud/service/LibroService.java
+++ src/main/java/com/biblioteca/bibliotecacrud/service/LibroService.java
@@ -1,4 +1,52 @@
 package com.biblioteca.bibliotecacrud.service;
 
+import com.biblioteca.bibliotecacrud.entity.Libro;
+import com.biblioteca.bibliotecacrud.repository.ILibroRepo;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class LibroService {
+
+    private ILibroRepo repo;
+
+    public List<Libro> Listar() {
+        return repo.findAll();
+    }
+
+    public Libro guardar(Libro libro){
+        return repo.save(libro);
+    }
+
+    public Libro actualizar(Long id, Libro libroBody){
+    Optional<Libro> libro = repo.findById(id);
+
+    Libro libroDb = libro.get();
+
+    if(libro.isPresent()) {
+    libroDb.setTitulo(libroBody.getTitulo());
+    libroDb.setNombresAutor(libroBody.getNombresAutor());
+    libroDb.setAnioPublicacion(libroBody.getAnioPublicacion());
+    libroDb.setDisponible(libroBody.getDisponible());
+    return repo.save(libroDb);
+    }
+        return repo.save(libroBody);
+    }
+
+    public Optional<Libro> obtenerUno(Long id) {
+        return repo.findById(id);
+    }
+
+    public void eliminar(Long id) {
+        repo.deleteById(id);
+    }
+
+    public List<Libro> guardarTodo(ArrayList<Libro> libros) {
+
+        return repo.saveAll(libros);
+    }
+
 }
