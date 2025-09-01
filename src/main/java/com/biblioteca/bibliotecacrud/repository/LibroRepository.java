558c4d60b061e726a3e73121e35774236e4b612a
SantiagoAlarconGutierrez <Santiago_Alarcongu@gmail.com>
2025-09-01 17:04:51 -0500

    Eliminacion de repositorio anterior

--- src/main/java/com/biblioteca/bibliotecacrud/repository/LibroRepository.java
+++
@@ -1,9 +0,0 @@
-package com.biblioteca.bibliotecacrud.repository;
-
-import com.biblioteca.bibliotecacrud.entity.Libro;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LibroRepository extends JpaRepository<Libro, Integer> {
-    //Verifica si hay un libro con el mismo titulo
-    boolean existsByLibro(String Libro);
-}
